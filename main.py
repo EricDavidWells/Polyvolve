f8891d22fb066c1c80110009b280bfcd48dc137d
philippeitis <33013301+philippeitis@users.noreply.github.com>
2019-12-17 18:23:44 -0800

    Tweaked file output, better CLI help.
    Only create directories if the user specifies that they want the image to be saved. Tweak the output file names, and add help strings to everything that I understood.

--- main.py
+++ main.py
@@ -65,33 +65,53 @@ def mutation_randshift(dna):
 def parse_args(init_functions, fitness_functions, crossover_functions, mutation_functions):
     import argparse
     parser = argparse.ArgumentParser()
-    parser.add_argument("--image_path", type=str, default="./starrynight.jpg")
-    parser.add_argument("--output_dir", type=str, default="./output/")
-    parser.add_argument("-x", type=int, default=650)
-    parser.add_argument("-y", type=int, default=None)
-    parser.add_argument("--display_x", type=int, default=300)
-    parser.add_argument("--display_y", type=int, default=None)
+    parser.add_argument("--image_path", type=str, default="./starrynight.jpg",
+                        help="Path to source image.")
+    parser.add_argument("--output_dir", type=str, default="./output/",
+                        help="Where output images should be stored.")
+    parser.add_argument("-x", type=int, default=650,
+                        help="Output image width.")
+    parser.add_argument("-y", type=int, default=None,
+                        help="Height of output image. If not set,"
+                             " automatically calculates using the input image's ratio.")
+    parser.add_argument("--display_x", type=int, default=300,
+                        help="Width of displayed image.")
+    parser.add_argument("--display_y", type=int, default=None,
+                        help="Height of displayed image. If not set,"
+                             " automatically calculates using the input image's ratio.")
     parser.add_argument("--calc_x", type=int, default=75)
     parser.add_argument("--calc_y", type=int, default=None)
-    parser.add_argument("--num_individuals", type=int, default=100)
-    parser.add_argument("--num_polygons", type=int, default=150)
-    parser.add_argument("--num_vertices", type=int, default=3)
-    parser.add_argument("--mutation_rate", type=float, default=0.01)
-    parser.add_argument("--mutation_amount", type=float, default=0.15)
-    parser.add_argument("--p_survival", type=float, default=0.15)
-    parser.add_argument("--p_reproduce", type=float, default=0.15)
+    parser.add_argument("--num_individuals", type=int, default=100,
+                        help="Population of individuals (aka images) present at each time step.")
+    parser.add_argument("--num_polygons", type=int, default=150,
+                        help="Number of polygons per individual")
+    parser.add_argument("--num_vertices", type=int, default=3,
+                        help="Number of vertices per polygon")
+    parser.add_argument("--mutation_rate", type=float, default=0.01,
+                        help="Rate at which individuals mutate.")
+    parser.add_argument("--mutation_amount", type=float, default=0.15,
+                        help="The degree to which an individual is mutated.")
+    parser.add_argument("--p_survival", type=float, default=0.15,
+                        help="Percentage of individuals that survive to the next step.")
+    parser.add_argument("--p_reproduce", type=float, default=0.15,
+                        help="Percentage of individuals which reproduce each step.")
     parser.add_argument("--init_fn", default="init_random", choices=init_functions.keys())
-    parser.add_argument("--fitness_fn", default="fitness_ssim", choices=fitness_functions.keys())
-    parser.add_argument("--crossover_fn", default="random_crossover", choices=["random_crossover"])
-    parser.add_argument("--mutation_fn", default="mutation_randshift", choices=["mutation_randshift"])
+    parser.add_argument("--fitness_fn", default="fitness_ssim", choices=fitness_functions.keys(),
+                        help="Function for determining the fitness of a given individual.")
+    parser.add_argument("--crossover_fn", default="random_crossover", choices=crossover_functions.keys(),
+                        help="Function for determining how individuals cross over (eg. what their children look like.)")
+    parser.add_argument("--mutation_fn", default="mutation_randshift", choices=mutation_functions.keys(),
+                        help="Function for mutating an individual.")
     parser.add_argument("-s", dest="save", action="store_true",
-                        help="Saves image to disk.")
+                        help="If selected, will periodically save the best individual, as an image, to diskm as well as"
+                             " a graph of fitness over time.")
     parser.add_argument("-p", dest="plot", action="store_true",
-                        help="Displays plot.")
+                        help="If selected, will display a plot of the program fitness as the program executes.")
     parser.add_argument("-d", dest="display", action="store_true",
-                        help="Displays image.")
+                        help="If selected, will periodically display the best individual.")
 
-    parser.add_argument("--minimize_fitness", action="store_true")
+    parser.add_argument("--minimize_fitness", action="store_true",
+                        help="If selected, chooses individuals with minimum fitness.")
     parser.add_argument("--max_generations", type=int, default=50000,
                         help="Maximum number of generations before the program finishes execution.")
     parser.add_argument("--generations_per_display", type=int, default=1,
@@ -103,15 +123,11 @@ def parse_args(init_functions, fitness_functions, crossover_functions, mutation_
     args = parser.parse_args()
     if not (args.display or args.plot or args.save):
         raise EnvironmentError("Must select one of -d, -p, or -s")
-    
+
     if args.plot:
         print("Please avoid dragging the plot until you see a line - the program may crash otherwise.")
 
     return args
-
-
-def handle_close(event):
-    exit(-1)
 
 
 if __name__ == "__main__":
@@ -133,15 +149,16 @@ if __name__ == "__main__":
                            "two_point_crossover": RicoGATools.twopointcrossover}
 
     args = parse_args(init_functions, fitness_functions, crossover_functions, mutation_functions)
+
     file_name = args.image_path
+
     output_directory = os.path.join(args.output_dir, os.path.basename(args.image_path).split(".")[0])
-
-    plot_dir = os.path.join(output_directory, PLOT_SUB_DIR)
-    os.makedirs(os.path.join(plot_dir), exist_ok=True)
-
-    im_width = args.x
     image_dir = os.path.join(output_directory, IMAGE_SUB_DIR)
-    os.makedirs(os.path.join(image_dir), exist_ok=True)
+    plot_dir = os.path.join(output_directory, PLOT_SUB_DIR)
+
+    if args.save:
+        os.makedirs(os.path.join(image_dir), exist_ok=True)
+        os.makedirs(os.path.join(plot_dir), exist_ok=True)
 
     num_individuals = args.num_individuals
     p_survival = args.p_survival
@@ -158,20 +175,23 @@ if __name__ == "__main__":
     mutation_rate = args.mutation_rate
     mutation_amount = args.mutation_amount
     mutation_function = mutation_functions[args.mutation_fn]
-    eval_type = 1  # 1 or maximizing fitness, -1 for minimizing fitness
+    eval_type = -1 if args.minimize_fitness else 1
 
     max_generations = args.max_generations
     generations_per_display = args.generations_per_display
+
+    fitness_log = []
     generations_per_plot = args.generations_per_plot
-    generations_per_save = args.generations_per_save
+
     num_images = 0
     len_count_str = len(str(max_generations // args.generations_per_save))
-    fitness_log = []
+    time_str = time.strftime("%Y%m%d_%H%M")
+    generations_per_save = args.generations_per_save
 
-    timestr = time.strftime("%Y%m%d_%H%M")
     img_orig = cv2.imread(file_name)
     shape_orig = img_orig.shape
     ratio = shape_orig[0] / shape_orig[1]
+
     disp_shape = (args.display_x, args.display_y if args.display_y else int(args.display_x * ratio))
     calc_shape = (args.calc_x, args.calc_y if args.calc_y else int(args.calc_x * ratio))
     save_shape = (args.x, args.y if args.y else int(args.x * ratio))
@@ -179,7 +199,8 @@ if __name__ == "__main__":
     img_calc = cv2.resize(img_orig, calc_shape)
     img_calc_enlarged = cv2.resize(img_calc, disp_shape)
 
-    cv2.imshow("target", img_calc_enlarged)
+    if args.display:
+        cv2.imshow("target", img_calc_enlarged)
 
     population = Population(num_individuals, init_function, fitness_function, crossover_function,
                             mutation_function, p_survival, p_reproduce, eval_type)
@@ -206,8 +227,10 @@ if __name__ == "__main__":
         if args.save and i % generations_per_save == 0:
             best.draw(save_shape)
             padded_str = str(num_images).zfill(len_count_str)
-            best.save(f"{image_dir}_GA{padded_str}.png")
-            plt.savefig(f"{plot_dir}_PL{padded_str}.png")
+            best.save(os.path.join(image_dir, f"{time_str}_GA{padded_str}.png"))
+
+            plt.plot(fitness_log, 'b')
+            plt.savefig(os.path.join(plot_dir, f"{time_str}_PL{padded_str}.png"))
             num_images += 1
 
         print(i)
